8da6451b4a90056e6199e6876be08bc12b1d3ee7
吕涛 <cheerlvtao@163.com>
2020-01-29 17:45:04 +0800

    APP下载页面

--- routes/view/AppDownload.js
+++ routes/view/AppDownload.js
@@ -2,7 +2,7 @@ const express = require('express')
 const  router = express.Router()
 
 router.get('/appDownload', (req, res, next) => {
-    res.render('appDowmload/index')
+    res.render('appDownload/index')
 })
 
 module.exports = router
